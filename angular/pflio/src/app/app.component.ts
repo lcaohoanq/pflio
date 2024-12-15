c8e6c886a99af3c8e758c66278da74c71908b4ea
lcaohoanq <hoangclw@gmail.com>
2024-12-15 15:32:21 +0700

    chore: add responsive

--- angular/pflio/src/app/app.component.ts
+++ angular/pflio/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProfileService } from "./services/profile.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pflio';
+export class AppComponent implements OnInit {
+  profile: any = null; // Initialize profile as null
+  informationURLMap: { [key: string]: string } = {};
+
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.profileService.getProfile().subscribe({
+      next: (data) => {
+        this.profile = data[0]; // Assuming the API returns a single profile in an array
+        this.informationURLMap = this.profile.socialLinks.reduce((acc: { [key: string]: string }, item: any) => {
+          acc[item.type] = item.url;
+          return acc;
+        }, {});
+      },
+      error: (error) => {
+        console.error('Error fetching profile data', error);
+      },
+      complete: () => {
+        console.log('Profile data fetch complete');
+      },
+    });
+  }
+
+  handleNavigate(name: string): void {
+    const url = this.informationURLMap[name];
+    if (url) {
+      window.open(url, '_blank');
+    }
+  }
 }
--- angular/pflio/src/app/app.module.ts
+++ angular/pflio/src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
