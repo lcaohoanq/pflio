9d419795de7ad39395383fa2efd0a194cca0bfbf
lcaohoanq <hoangclw@gmail.com>
2025-11-20 15:56:10 +0700

    feat: add n8n webhook

--- react-appwrite/src/App.tsx
+++ react-appwrite/src/App.tsx
@@ -4,6 +4,17 @@ import Portfolio from "./pages/Portfolio";
 import { ThemeProvider } from "./components/ThemeProvider";
 
 function App() {
+  React.useEffect(() => {
+    fetch(import.meta.env.VITE_N8N_PORTFOLIO_VISIT_WEBHOOK_URL! ?? "hehe", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        ua: navigator.userAgent,
+        time: new Date().toISOString(),
+      }),
+    }).catch(() => {});
+  }, []);
+
   return (
     <ThemeProvider>
       <Router>
